510e0e18a9f1782eadb729bf3a13704d39c7a673
michelene <michelenechon@gmail.com>
2020-02-05 11:47:31 -0800

    Integrate react-youtube NPM package

--- src/components/MovieTrailer.js
+++ src/components/MovieTrailer.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
-function MovieTrailer({ movie }) {
-  return <div className="MovieTrailer">MovieTrailer</div>;
+// function MovieTrailer({ movie }) {
+//   return <div className="MovieTrailer">MovieTrailer</div>;
+// }
+
+class MovieTrailer extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+
+    return (
+      <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
+    );
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
 }
 
 export default MovieTrailer;
