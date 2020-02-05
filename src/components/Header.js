2eba08be4efea48f59d071981713cc45c278b2bf
michelene <michelenechon@gmail.com>
2020-02-05 09:10:50 -0800

    Initial styling of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Header() {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      // TODO: Use a movie reel SVG for favicon and inside header
+      <h1>What's Playing?</h1>
+    </div>
+  );
 }
 
 export default Header;
